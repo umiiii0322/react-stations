f6ddeb4ed367ed54258f45ea71188030d39cfdf0
umiiii0322 <unno.shota1222@gmail.com>
2024-07-02 22:37:13 +0900

    3-3 まで終了

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@
 
 import './App.css'
 
+
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
   return (
     <div>
-      <h2>Hello, world!</h2>
+    <header>
+      Dogアプリ
+    </header>
+    <body>
+      <p>犬の画像を表示するサイトです。</p>
+      <img src='https://images.dog.ceo/breeds/shiba/mamehiko03.jpg' alt='犬の画像'/>
+    </body>
     </div>
   )
 }
